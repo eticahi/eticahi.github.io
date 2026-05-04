731391a779f6e0a16ca5cccd61acd7e70d335cd9
eticahi <lucasadietv@gmail.com>
2026-05-04 20:53:19 +0200

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -6,7 +6,7 @@
  */
 
 const ADMIN_USER = "eticahi";
-const ADMIN_PASS = "oir";
+const ADMIN_PASS = "eticahi";
 const SESSION_KEY = "mn_gov_admin_session";
 // LS_PREFIX is defined in data-loader.js (loaded earlier on admin.html);
 // re-declaring it here would throw SyntaxError. Reuse the global.
